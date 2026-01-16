157ec17a584667ab975f9dd3e42d927e376c2cb5
Pradip Magadum <pradipmagadum@gmail.com>
2026-01-16 09:54:58 +0530

    Initial commit

---
+++ MiniProjectOOPS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniProjectOOPS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My first Git project");
+        }
+    }
+}
